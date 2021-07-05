63f583072da09dc6c80afe84368247c89f854116
Vladimir Kilinich <kylynych.v@gmail.com>
2021-07-06 01:27:40 +0300

    Initial commit

---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState, useCallback } from 'react';
+import { interval, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+const Timer = () => {
+  const [timerSec, setTimerSec] = useState(0);
+  const [timerStatus, setTimerStatus] = useState('stop');
+  const [clickCount, setClickCount] = useState(0);
+
+  useEffect(() => {
+    const timer = new Subject();
+    interval(1000)
+      .pipe(takeUntil(timer))
+      .subscribe(() => {
+        if (timerStatus === 'run') {
+          setTimerSec(time => time + 1000);
+        }
+      });
+
+    return () => {
+      timer.next();
+      timer.complete();
+    };
+  }, [timerStatus]);
+
+  const start = useCallback(() => {
+    setTimerStatus('run');
+  }, []);
+
+  const stop = useCallback(() => {
+    setTimerStatus('stop');
+    setTimerSec(0);
+  }, []);
+
+  const reset = useCallback(() => {
+    setTimerSec(0);
+  }, []);
+
+  const wait = useCallback(() => {
+    let singleClickTimer;
+
+    setClickCount(prevClickCount => prevClickCount + 1);
+
+    if (clickCount === 0) {
+      singleClickTimer = setTimeout(function () {
+        setClickCount(0);
+      }, 300);
+    }
+
+    if (clickCount === 1) {
+      setTimerStatus('wait');
+
+      clearTimeout(singleClickTimer);
+      setClickCount(0);
+    }
+
+    console.log(clickCount);
+  }, [clickCount]);
+
+  return (
+    <div>
+      <span> {new Date(timerSec).toISOString().slice(11, 19)}</span>
+      {timerStatus === 'run' ? (
+        <button className='stop-button' onClick={stop}>
+          Stop
+        </button>
+      ) : (
+        <button className='start-button' onClick={start}>
+          Start
+        </button>
+      )}
+      <button onClick={reset}>Reset</button>
+      <button onClick={wait}>Wait</button>
+    </div>
+  );
+};
+
+export default Timer;
